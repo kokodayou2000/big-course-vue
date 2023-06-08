4fdbef7d4b7899fbe672c25aaf256a7609f75f49
kokodayou2000 <1611601667@qq.com>
2023-06-08 23:31:57 +0800

    关于

---
+++ src/views/about/AboutView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <el-row :gutter="12">
+    <el-col :span="8">
+      <el-card shadow="hover"> Vue </el-card>
+    </el-col>
+
+    <el-col :span="8">
+      <el-card shadow="hover"> TypeScript </el-card>
+    </el-col>
+
+    <el-col :span="8">
+      <el-card shadow="never"> Never </el-card>
+    </el-col>
+  </el-row>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import {Ship} from "@element-plus/icons-vue";
+</script>
+
+<template>
+<div class="home">
+  <h1>home</h1>
+  <el-icon>
+    <Ship />
+  </el-icon>
+</div>
+</template>
+
+<style scoped>
+
+</style>
